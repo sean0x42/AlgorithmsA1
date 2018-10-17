78a26aea83a8d66960fb5e6d18a9f8d66c93235c
Sean Bailey <sean@seanbailey.io>
2018-10-17 22:27:24 +1100

    Validate command line arguments.

--- io/seanbailey/railnetwork/Main.java
+++ io/seanbailey/railnetwork/Main.java
@@ -1,5 +1,7 @@
 package io.seanbailey.railnetwork;
 
+import java.io.File;
+
 import io.seanbailey.railnetwork.util.Logger;
 
 /**
@@ -27,6 +29,16 @@ public class Main {
       printUsage();
       return;
     }
+
+    // Input
+    File file = new File(args[0]);
+    String origin = args[1];
+    String destination = args[2];
+
+    // Validate file
+    if (!isDataFileValid(file, args[0])) {
+      return;
+    }
   }
 
   /**
@@ -35,5 +47,34 @@ public class Main {
   private static void printUsage() {
     logger.info("Usage: java assign1 <dataFile> <origin> <destination> " +
         "[criterion]");
+  }
+
+  /**
+   * Determines whether the given file could be a valid data file. Note that 
+   * this method does not perform any validation on the contents of the file.
+   * @param file Data file to validate.
+   * @param path Path to file. For error messages.
+   * @return Whether the file is valid.
+   */
+  private static boolean isDataFileValid(File file, String path) {
+    // Make sure file exists
+    if (!file.exists()) {
+      logger.error("File not found '%s'.", path);
+      return false;
+    }
+
+    // Ensure file is not a directory
+    if (file.isDirectory()) {
+      logger.error("Data file should not be a directory.");
+      return false;
+    }
+
+    // Ensure the file is readable
+    if (!file.canRead()) {
+      logger.error("Data file cannot be read.");
+      return false;
+    }
+
+    return true;
   }
 }
