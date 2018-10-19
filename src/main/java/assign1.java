e9db96e2a6f6844ad62ad36244bfb73e20f3214c
Sean Bailey <sean@seanbailey.io>
2018-10-19 12:57:11 +1100

    Added gradle.

---
+++ src/main/java/assign1.java
@@ -0,0 +1,31 @@
+import io.seanbailey.railnetwork.RailNetwork;
+
+/**
+ * Entry point to the application. This essentially acts as an interface to 
+ * @link{io.seanbailey.railnetwork.RailNetwork}.
+ *
+ * <p>
+ * This program finds the shortest path (based on travel time) between a two
+ * locations of a Rail Network. 
+ *
+ * Input:
+ * <ul>
+ *   <li><strong>dataFile:</strong> An XML file containing each station in the
+ *   rail network, along with their connected stations and lines.</li>
+ *   <li><strong>origin:</strong> Starting station.</li>
+ *   <li><strong>destination:</strong> Ending station.</li>
+ * </ul>
+ * </p>
+ *
+ * @author Sean Bailey c3279343
+ */
+public class assign1 {
+
+  /**
+   * Entry point.
+   * @param args An array of command line args from STDIN.
+   */
+  public static void main(String[] args) {
+    RailNetwork.main(args);
+  }
+}
---
+++ src/main/java/io/seanbailey/railnetwork/exception/ValidationException.java
@@ -0,0 +1,26 @@
+package io.seanbailey.railnetwork.exception;
+
+/**
+ * An exception that can be thrown whenever validation of a certain resource
+ * fails.
+ * @author Sean Bailey c3279343
+ */
+public class ValidationException extends Throwable {
+  
+  /**
+   * Constructs a new validation exception.
+   * @param message Exception message.
+   */
+  public ValidationException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new validation exception.
+   * @param format Message format.
+   * @param values Values to populate format with.
+   */
+  public ValidationException(String format, Object... values) {
+    this(String.format(format, values));
+  }
+}
